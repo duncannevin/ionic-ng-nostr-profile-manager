e17c9c82f402055a31e0f557955f9a382f3f7180
duncannevin <duncan.nevin@gmail.com>
2023-05-10 18:37:58 -0700

    Stub metadata ui.

--- projects/nostr-profile/src/lib/metadata/metadata.component.ts
+++ projects/nostr-profile/src/lib/metadata/metadata.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'nostr-profile-metadata',
   templateUrl: './metadata.component.html',
   styleUrls: ['./metadata.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule, FormsModule, IonicModule]
 })
-export class MetadataComponent {
+export class MetadataComponent implements OnInit {
+  isWeb = false;
 
+  ngOnInit(): void {
+    this.isWeb = this.platform.is('mobileweb') || !this.platform.is('mobile');
+  }
+
+  constructor(private platform: Platform) {}
 }
